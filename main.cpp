d4f6d9dcf53dbf08556378ef419c8e01598d5580
paulouis <paulouis.gomichel@gmail.com>
2016-11-17 09:39:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include <osg/Vec3f>
+#include "Avion.h"
+#include "Joueur.h"
+#include "Ennemi.h"
+using namespace std ;
+
+
+
+
+int main(){
+
+    Joueur J1 = Joueur();
+    Joueur J2 = Joueur();
+    Ennemi E1 = Ennemi();
+    Ennemi E2 = Ennemi();
+   J1.setPosition = osg::Vec3f(0,5,0);
+    J1.setDirection = osg::Vec3f(1,0,0);
+    J2.setPosition = osg::Vec3f(0,8,0);
+    J2.setDirection = osg::Vec3f(1,0,0);
+    E1.setPosition = osg::Vec3f(13,5,13);
+    E1.setDirection = osg::Vec3f(-1,0,0);
+    E2.setPosition = osg::Vec3f(13,8,13);
+    E2.setDirection = osg::Vec3f(-1,0,0);
+
+
+
+}
+
